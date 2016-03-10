43a3c4455abbcad0e3bd2b8df54558decc888a9a
ChadClark_Dev <chadclarke@bell.net>
2016-03-10 10:43:25 -0500

    console log update

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,7 +3,13 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
     
+    "use strict";
     
+    var person = {}; /*var person = new object();*/
+    
+    Person.age = 40; 
+    
+    console.log(Person.age);
     
     
 })();
